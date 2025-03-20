3e1aa03e2182fe0a077a288e1d940f1174fe1b52
jackjavi <jackmtembete@gmail.com>
2025-03-20 13:20:10 +0300

    updates

--- frontend/components/footer-2.jsx
+++ frontend/components/footer-2.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className=" text-gray-800 h-[25vh] px-6 bg-gray-500 bg-opacity-50 flex flex-col w-full z-50">
+    <footer className=" text-gray-800 md:h-[25vh] px-6 bg-gray-500 bg-opacity-50 flex flex-col w-full z-50">
       {/* Top Section */}
-      <div className="flex justify-center items-center  py-4 space-x-10 ">
+      <div className="hidden md:flex justify-center items-center  py-4 space-x-10 ">
         <a href="#" className="hover:underline bg-gray-300 p-4 px-10">
           Blog
         </a>
@@ -17,11 +17,11 @@ const Footer = () => {
       </div>
 
       {/* Middle Section */}
-      <div className="bg-gray-100 py-2 text-center text-sm flex items-center justify-center grow">
+      <div className="bg-gray-100 p-2 mt-6 md:mt-0 text-center text-sm flex items-center justify-center grow">
         <div className="text-sm flex flex-col items-center justify-center gap-2">
           <p>ELITE BRAINS CONSULTING</p>
 
-          <div className="flex justify-center items-center space-x-4 w-full">
+          <div className="hidden md:flex justify-center items-center space-x-4 w-full">
             <p>EMAIL: elitebrainsconsulting@gmail.com</p>
             <p>30367-00100 NAIROBI, KENYA</p>
           </div>
@@ -29,7 +29,7 @@ const Footer = () => {
       </div>
 
       {/* Bottom Section */}
-      <div className="bg-gray-100 py-2 mb-6 text-center text-xs  ">
+      <div className="hidden md:block bg-gray-100 py-2 mb-6 text-center text-xs  ">
         <p className="flex gap-4 justify-center items-center">
           <span>Copyright © 2025 EBC.™ All rights reserved.</span>•
           <a href="#" className="hover:underline">
